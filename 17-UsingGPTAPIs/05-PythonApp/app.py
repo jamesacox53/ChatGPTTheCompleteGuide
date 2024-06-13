a0546d86bee111c94a1f32eabe6cd6ac35295844
jamesacox53 <jamesacox53@gmail.com>
2024-06-13 17:59:58 +0100

    Completed Creating our First Python App Using the API

---
+++ 17-UsingGPTAPIs/05-PythonApp/app.py
@@ -0,0 +1,11 @@
+import openai
+openai.api_key = "OPENAI_API_KEY"
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo"
+    messages=[
+        {"role":"user", "content":"Hello!"}
+    ]
+)
+
+print(completion.choices[0].message)
