be569d85cf66cec0d0dae40db20dcb0d15472650
jamesacox53 <jamesacox53@gmail.com>
2024-06-14 10:13:08 +0100

    Completed Understanding the API Request

---
+++ 17-UsingGPTAPIs/06-UnderstandingRequest/app.py
@@ -0,0 +1,13 @@
+import openai
+openai.api_key = "OPENAI_API_KEY"
+
+user_input = input("Add your prompt: ")
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo"
+    messages=[
+        {"role":"user", "content": user_input}
+    ]
+)
+
+print(completion.choices[0].message)
