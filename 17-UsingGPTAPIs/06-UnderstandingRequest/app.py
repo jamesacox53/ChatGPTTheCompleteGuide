89aa53db4a6f4e08fa2aa30b13c056dd352cd753
jamesacox53 <jamesacox53@gmail.com>
2024-06-14 11:18:40 +0100

    Completed Adding Parameters

--- 17-UsingGPTAPIs/06-UnderstandingRequest/app.py
+++ 17-UsingGPTAPIs/06-UnderstandingRequest/app.py
@@ -4,7 +4,7 @@ openai.api_key = "OPENAI_API_KEY"
 user_input = input("Add your prompt: ")
 
 completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo"
+    model="gpt-3.5-turbo",
     messages=[
         {"role":"user", "content": user_input}
     ]
--- 17-UsingGPTAPIs/07-AddingParameters/app.py
+++ 17-UsingGPTAPIs/07-AddingParameters/app.py
@@ -1,11 +1,15 @@
 import openai
 openai.api_key = "OPENAI_API_KEY"
 
+user_input = input("Add your prompt: ")
+
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role":"user", "content":"Hello!"}
-    ]
+        {"role":"user", "content": user_input}
+    ],
+    temperature=0.2,
+    max_tokens=100
 )
 
 print(completion.choices[0].message)
