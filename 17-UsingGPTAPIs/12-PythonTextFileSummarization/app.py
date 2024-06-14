ab82b01f1ee2a92bec21088f5351a63ea51192a5
jamesacox53 <jamesacox53@gmail.com>
2024-06-14 17:04:02 +0100

    Completed Processing Text From Files & Create New Files with API Generated Content

---
+++ 17-UsingGPTAPIs/12-PythonTextFileSummarization/app.py
@@ -0,0 +1,22 @@
+import openai
+openai.api_key = "OPENAI_API_KEY"
+
+input_text = ''
+
+with open('text.txt', 'r') as file:
+    input_text = file.read()
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role":"system", "content": "You are a text summarizer chat bot. Your goal is to summarize the text that is given by the user."},
+        {"role":"user", "content": input_text}
+    ],
+    temperature=0
+)
+
+ai_response = response.choices[0].message.content
+print("Summarized text:\n\n", ai_response)
+
+with open('summary.txt', 'w') as file:
+    file.write(ai_response)
